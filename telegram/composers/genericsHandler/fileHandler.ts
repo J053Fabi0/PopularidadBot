4ae9cadc042e27d5d7cd3ac21f11050db4963e78
Jose Fabio <J053Fabi0@users.noreply.github.com>
2024-04-01 17:16:17 -0600

    Files handler to restore backup files

---
+++ telegram/composers/genericsHandler/fileHandler.ts
@@ -0,0 +1,31 @@
+import axiod from "axiod";
+import db from "../../../data/database.ts";
+import { Context, Filter } from "grammy/mod.ts";
+import { ADMIN_ID, BOT_TOKEN } from "../../../env.ts";
+import { dataToBackup } from "../commandsHandler/backupCommand.ts";
+
+interface FileData {
+  key: (typeof dataToBackup)[number];
+  // deno-lint-ignore no-explicit-any
+  value: any[];
+}
+
+export default async function fileHandler(ctx: Filter<Context, "message:file">) {
+  if (ctx.from?.id !== ADMIN_ID) return;
+
+  const name = ctx.message?.document?.file_name;
+  if (!name) return;
+
+  const file = await ctx.getFile();
+  const { data } = await axiod.get(`https://api.telegram.org/file/bot${BOT_TOKEN}/${file.file_path}`, {
+    responseType: "arraybuffer",
+  });
+
+  // parse data to object
+  const obj = JSON.parse(new TextDecoder().decode(data)) as FileData;
+
+  await db[obj.key].deleteMany();
+  const { ok } = await db[obj.key].addMany(obj.value);
+
+  await ctx.reply(`<code>${name}: ${ok}</code>`, { parse_mode: "HTML" });
+}
